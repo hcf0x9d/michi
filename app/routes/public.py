e8de46b5fc67687ab44f2abc42f82d18d076f530
jfukura <jason@jasonfukura.com>
2025-05-26 10:14:40 -0700

    dynamic sitemap

--- app/routes/public.py
+++ app/routes/public.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, abort
+from datetime import datetime
+
+from flask import Blueprint, render_template, abort, url_for, Response
 
 from app.services.hygraph import fetch_resources, fetch_resource_by_slug
 
@@ -36,3 +38,50 @@ def about():
 def contact():
     masthead_cta = True
     return render_template("views/contact.html", **locals())
+
+
+@public_bp.route('/sitemap.xml', methods=['GET'])
+def sitemap():
+    pages = []
+    lastmod = datetime.now().date().isoformat()
+
+    # Static pages
+    static_pages = [
+        {'endpoint': 'public.home', 'changefreq': 'monthly', 'priority': 0.9},
+        {'endpoint': 'public.services', 'changefreq': 'monthly', 'priority': 1.0},
+        {'endpoint': 'public.contact', 'changefreq': 'monthly', 'priority': 0.3},
+        {'endpoint': 'public.about', 'changefreq': 'monthly', 'priority': 0.8},
+        {'endpoint': 'public.resources', 'changefreq': 'monthly', 'priority': 0.9},
+    ]
+
+    for page in static_pages:
+        url = url_for(page['endpoint'], _external=True)
+        pages.append(f"""
+        <url>
+            <loc>{url}</loc>
+            <lastmod>{lastmod}</lastmod>
+            <changefreq>{page['changefreq']}</changefreq>
+            <priority>{page['priority']}</priority>
+        </url>
+        """)
+
+    # Dynamic resource pages
+    resources = fetch_resources()
+    for resource in resources:
+        url = url_for('public.resource_detail', slug=resource['slug'], _external=True)
+        pages.append(f"""
+        <url>
+            <loc>{url}</loc>
+            <lastmod>{lastmod}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.7</priority>
+        </url>
+        """)
+
+    # Compile full XML
+    sitemap_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        {''.join(pages)}
+    </urlset>"""
+
+    return Response(sitemap_xml, mimetype='application/xml')
